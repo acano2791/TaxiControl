7efecc79fe5d8d4671a09e50585c800a4843b00d
acano2791 <acano2791@gmail.com>
2026-09-06 23:04:50 -0600

    feat: implementar contexto de tema claro y oscuro

--- App.tsx
+++ App.tsx
@@ -2,17 +2,33 @@ import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./src/navigation/StackNavigator";
 import { navigationRef } from "./src/navigation/NavigationService";
 import { AuthProvider } from "./src/contexts/AuthContext";
+import { ThemeProvider, useTheme } from "./src/contexts/ThemeContext";
+import { StatusBar } from "expo-status-bar";
+
+function AppNavigation() {
+  const { isDark } = useTheme();
+  return (
+    <>
+      <StatusBar style={isDark ? "light" : "dark"} />
+      <NavigationContainer ref={navigationRef}>
+        {/* Contenedor principal que administra la navegación. */}
+        <StackNavigator />
+        {/* Stack Navigator que contiene las pantallas principales. */}  
+      </NavigationContainer>
+    </>
+  );
+}
 
 export default function App() {
   return (
-    // AuthProvider permite compartir la sesión del usuario
+    // ThemeProvider permite compartir el tema
     // con todas las pantallas de la aplicación.
-    <AuthProvider>
-      {/* Contenedor principal que administra la navegación. */}
-      <NavigationContainer ref={navigationRef}>
-        {/* Stack Navigator que contiene las pantallas principales. */}
-        <StackNavigator />
-      </NavigationContainer>
+    <ThemeProvider>
+    {/* AuthProvider permite compartir la sesión del usuario
+          con todas las pantallas de la aplicación. */}
+    <AuthProvider>      
+      <AppNavigation />                 
     </AuthProvider>
+    </ThemeProvider>
   );
 }
